b7d738132cc5c3c59a9191b5826f050ef5ff09b8
Tisaei <48901126+Tisaei@users.noreply.github.com>
2021-02-19 15:28:45 +0900

    プレイヤー(Candy)の配置,

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    enum eDir
+    {
+        Left,   //左
+        Up,     //上
+        Right,  //右
+        Down    //下
+    }
+}
