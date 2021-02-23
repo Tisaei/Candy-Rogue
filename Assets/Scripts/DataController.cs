a22c0814b56171cf50d7207e31c52b1d576117e8
Tisaei <48901126+Tisaei@users.noreply.github.com>
2021-02-24 00:41:58 +0900

    マップのセーブ・ロード機能の追加

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -1,18 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.IO;
 
 public class DataController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject TileMap;
+    private TilemapController TilemapC;
+
+    [System.Serializable]
+    public class Data
+    {
+        public dataX[] mapDataX;
+        [System.Serializable]
+        public struct dataX
+        {
+            public eMapGimmick[] dataY;
+        }
+    }
+    private Data SaveData = new Data();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TilemapC = TileMap.GetComponent<TilemapController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Data data = Load();
+            eMapGimmick[,] mapData = new eMapGimmick[data.mapDataX.Length, data.mapDataX[0].dataY.Length];
+            for (int x = 0; x < mapData.GetLength(0); x++)
+            {
+                for (int y = 0; y < mapData.GetLength(1); y++)
+                {
+                    mapData[x, y] = data.mapDataX[x].dataY[y];
+                }
+            }
+            TilemapC.LoadArray(mapData);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Array2D data = TilemapC.GetArray2D();
+            SaveData.mapDataX = new Data.dataX[data.GetWidth()];
+            for(int x = 0; x < data.GetWidth(); x++)
+            {
+                SaveData.mapDataX[x].dataY = new eMapGimmick[data.GetHeight()];
+                for (int y = 0; y < data.GetHeight(); y++)
+                {
+                    SaveData.mapDataX[x].dataY[y] = data.Get(x, y);
+                }
+            }
+            Save();
+            Debug.Log("Saved");
+        }
+    }
+
+    private void Save()
+    {
+        string jsonstr = JsonUtility.ToJson(SaveData);
+        Debug.Log("Saving\n"+jsonstr);
+        var writer = new StreamWriter(Application.dataPath + "/save.json", false);
+        writer.Write(jsonstr);
+        writer.Flush();
+        writer.Close();
+    }
+    private Data Load()
+    {
+        StreamReader reader = new StreamReader(Application.dataPath + "/save.json");
+        string datastr = reader.ReadToEnd();
+        reader.Close();
+        Data data = JsonUtility.FromJson<Data>(datastr);
+        return data;
     }
 }
--- Assets/Scripts/TilemapController.cs
+++ Assets/Scripts/TilemapController.cs
@@ -7,6 +7,7 @@ using UnityEngine.Tilemaps;
 public class TilemapController : MonoBehaviour
 {
     private Tilemap Tilemap;
+    private Transform TilemapTransform;
 
     [SerializeField]
     private TileBase FloorTile;
@@ -16,39 +17,64 @@ public class TilemapController : MonoBehaviour
     private TileBase WallTile;
 
     [SerializeField]
-    private int mapWidth;
+    private int mapWidth = 25;
     [SerializeField]
-    private int mapHeight;
+    private int mapHeight = 25;
+
+    [SerializeField]
+    private int charactorDots = 16;
 
     private Array2D mapData;
+
     // Start is called before the first frame update
     void Start()
     {
         Tilemap = GetComponent<Tilemap>();
         Tilemap.ClearAllTiles();
-        mapData = new Array2D(mapWidth, mapHeight);
+        TilemapTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GnerateArray();
+        }
     }
 
     public void GnerateArray()
     {
+        mapData = new Array2D(mapWidth, mapHeight);
         // ローグライクのダンジョンマップを自動生成する.
+        bool placedStair = false;
         for (int x = 0; x < mapData.GetWidth(); x++)
         {
             for (int y = 0; y < mapData.GetHeight(); y++)
             {
-                mapData.Set(x, y, 0);
+                eMapGimmick v;
+                if (placedStair)
+                {
+                    v = (eMapGimmick)Enum.ToObject(typeof(eMapGimmick), UnityEngine.Random.Range(0, 2));
+                }
+                else
+                {
+                    v = (eMapGimmick)Enum.ToObject(typeof(eMapGimmick), UnityEngine.Random.Range(0, 3));
+                }
+                if (v == eMapGimmick.Stair)
+                {
+                    placedStair = true;
+                }
+                mapData.Set(x, y, v);
             }
         }
+        RenderMap();
+        Debug.Log("Generatd");
     }
 
-    public void LoadArray(int[,] loadData)
+    public void LoadArray(eMapGimmick[,] loadData)
     {
+        mapData = new Array2D(loadData.GetLength(0), loadData.GetLength(1));
         // セーブデータからダンジョンマップをロードする.
         for (int x = 0; x < mapData.GetWidth(); x++)
         {
@@ -57,9 +83,16 @@ public class TilemapController : MonoBehaviour
                 mapData.Set(x, y, loadData[x, y]);
             }
         }
+        RenderMap();
+        Debug.Log("Loaded");
     }
 
-    public void RenderMap()
+    public Array2D GetArray2D()
+    {
+        return mapData;
+    }
+
+    private void RenderMap()
     {
         // ダンジョンマップからTilemapを生成する.
 
@@ -84,6 +117,7 @@ public class TilemapController : MonoBehaviour
             }
         }
     }
+
 
     public Pos2D CoordinateMoveTo(Pos2D position, Vec2D vec)
     {
@@ -122,6 +156,28 @@ public class TilemapController : MonoBehaviour
         }
         return new Pos2D((int)Math.Round(nowX), (int)Math.Round(nowY));
     }
+
+    int ToWorldX(int xgrid)
+    {
+        return charactorDots * xgrid + charactorDots / 2 + (int)TilemapTransform.position.x;
+    }
+    int ToWorldY(int ygrid)
+    {
+        return charactorDots * ygrid + charactorDots / 2 + (int)TilemapTransform.position.y;
+    }
+    int ToGridX(int xworld)
+    {
+        return (xworld - (int)TilemapTransform.position.x - charactorDots / 2) / charactorDots;
+    }
+    int ToGridY(int yworld)
+    {
+        return (yworld - (int)TilemapTransform.position.y - charactorDots / 2) / charactorDots;
+    }
+    bool IsCollide(Pos2D position) // 指定の座標が壁かどうかをチェック.
+    {
+        var gimmick = mapData.Get(position.x, position.y);
+        return (gimmick == eMapGimmick.Wall || gimmick == eMapGimmick.Null);
+    }
 }
 
 public class Array2D
@@ -133,41 +189,27 @@ public class Array2D
     }
     public eMapGimmick Get(int x, int y)
     {
-        if (x >= map.GetUpperBound(0) || y >= map.GetUpperBound(1) || x < 0 || y < 0)
+        if (x >= map.GetLength(0) || y >= map.GetLength(1) || x < 0 || y < 0)
         {
             return eMapGimmick.Null;
         }
         return map[x, y];
     }
-    public int Set(int x, int y, int v)
+    public int Set(int x, int y, eMapGimmick v)
     {
-        if (x >= map.GetUpperBound(0) || y >= map.GetUpperBound(1) || x < 0 || y < 0)
+        if (x >= map.GetLength(0) || y >= map.GetLength(1) || x < 0 || y < 0)
         {
             return -1;
         }
-        switch (v)
-        {
-            case 0:
-                map[x, y] = eMapGimmick.Wall;
-                break;
-            case 1:
-                map[x, y] = eMapGimmick.Floor;
-                break;
-            case 2:
-                map[x, y] = eMapGimmick.Stair;
-                break;
-            default:
-                map[x, y] = eMapGimmick.Null;
-                break;
-        }
+        map[x, y] = v;
         return 0;
     }
     public int GetWidth()
     {
-        return map.GetUpperBound(0);
+        return map.GetLength(0);
     }
     public int GetHeight()
     {
-        return map.GetUpperBound(1);
+        return map.GetLength(1);
     }
 }
